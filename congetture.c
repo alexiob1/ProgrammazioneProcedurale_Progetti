47dcf372b94e50a4a1061a195e1763ef66a8bb1a
Alessio Biagi <120371344+alexiob1@users.noreply.github.com>
2023-04-12 22:40:38 +0200

    Add files via upload
    In questa versione, il nome del file sorgente è stato modificato per comodità.
    All'interno del file sorgente è stato implementata la funzione per la scelta e inserito nel main l'instradamento delle istruzioni, attraverso uno switch.

---
+++ congetture.c
@@ -0,0 +1,159 @@
+/************************************************************************/
+/*  ESAME DI PROGRAMMAZIONE PROCEDURALE - SESSIONE ESTIVA 2023          */					   
+/*                                                                      */					   
+/* Programma che, in base alla scelta eseguita dall'utente, 	        */
+/* esegue le congetture di Gilbreath, debole di Goldbach e Legendre.    */
+/*                                                                      */                                   
+/* Autori:  Biagi Alessio (Matricola: 322735)                           */
+/*          Fabbri Nicole (Matricola: 321119)                           */
+/************************************************************************/
+
+/*****************************/
+/* Inclusione delle librerie */
+/*****************************/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/********************************/
+/* Dichiarazione delle funzioni */
+/********************************/
+int selezione();
+void gilbreath(void);
+void goldbach(void);
+void legendre(void);
+int numprim(int);
+
+/***********************************/
+/* Definizione delle funzioni      */
+/***********************************/
+
+/* Definizione della funzione main */
+int main()
+{
+	/*Dichiarazione delle variabili globali*/
+	int congettura;
+
+	/*Richiamo della funzione di selezione*/
+	congettura = selezione();
+
+	switch(congettura)
+	{
+		case 1:
+		{
+			gilbreath();
+			break;
+		}
+
+		case 2:
+		{
+			goldbach();
+			break;
+		}
+
+		case 3:
+		{
+			legendre();
+			break;
+		}
+	}
+
+return(0);
+}
+
+int selezione()
+{
+	/*Dichiarazione delle variabili globali*/
+	int esito_lettura, 
+	    acquisizione_errata,
+	    scelta;
+
+	do
+	{
+		printf("Quale congettura vuoi verificare?\n"
+		"1. Congettura di Gilbreath\n"
+		"2. Congettura debole di Goldbach\n"
+		"3. Congettura di Legendre\n"
+		"Inserisci il numero della congettura\n");
+		esito_lettura = scanf("%d",
+	              		      &scelta);
+
+		acquisizione_errata=esito_lettura=scelta>3||scelta<=0;
+		if (acquisizione_errata)
+			printf("Il valore inserito non è valido\n");
+		while(getchar() != '\n');
+	}
+	while(acquisizione_errata);
+
+return(scelta);
+}
+
+void gilbreath(void)
+{
+
+
+}
+
+void goldbach(void)
+{
+	int n,	    /*Input: Numero inserito dall'utente*/
+	    i,
+	    j,
+	    k,
+	    esito_lettura,
+	    acquisizione_errata,
+	    esito;
+	do
+	{
+		printf("Inserire un numero dispari e >5\n");
+		esito_lettura = scanf("%d",
+	      		              &n);
+		
+		acquisizione_errata = esito_lettura != 1 || n <= 5 || n % 2 == 0;
+		if (acquisizione_errata)
+			printf("Il valore inserito non è valido\n");
+		while(getchar() != '\n');
+	}
+	while(acquisizione_errata);
+
+	for(i = 2; i <= n/3; i++)
+	{
+		esito = numprim(i);
+		if (esito == 1)
+		{
+			for(j = i; j <= n/2; j++)
+			{
+				esito = numprim(j);
+				if (esito == 1)
+				{
+					k = n - i -j;
+					
+					esito = numprim(k);
+					if (esito == 1)
+						printf("%d + %d + %d = %d\n",
+						       i, j, k, n);
+				}
+			}
+		}
+	}
+}
+
+void legendre(void)
+{
+	
+}
+
+int numprim(int n)
+{
+	int cdiv = 2,
+	    esito = 1; /* 1 = primo, 0 = non primo*/
+	
+	while(cdiv <= n/2)
+	{
+		if(n % cdiv == 0)
+			esito = 0; /* il numero non è primo */
+		cdiv++;
+	}
+
+return(esito);
+}
